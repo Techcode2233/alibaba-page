ea5093006380b4ad48c4f435f15ae77d357bc953
Xkill119966 <devthureinhtun@gmail.com>
2024-06-01 01:55:38 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins'],
+        W02ArtHouse: ['W02ArtHouse'],
+        W23ArtHouse: ['W23ArtHouse'],
+      },
+    },
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+
+  },
+  plugins: [],
+}
